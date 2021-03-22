e6c79c45b5c0fe8d9145741990ab0bc47c3b5348
Fernando Soto <fjsoto1199@gmail.com>
2021-03-22 17:07:53 -0500

    Added superflip to tests

--- ScrambleTest.py
+++ ScrambleTest.py
@@ -11,7 +11,8 @@ class MyTestCase(unittest.TestCase):
                      ('O', 'G', "U' B2 F' R' D U2 R2 D' F' R' D2 L' F2 R L B R2 U' R' U R' B2 F U2 B'"),
                      ('Y', 'B', "D' F' D B' L U F2 U' R L' U' B2 R F B2 U D2 B F L' D2 L2 B F U2 R D2 U2 B2 L R' B R' U' D F2 B2 L' B' D' U L' F' D L' U' L2 U2 B F2"),
                      ('O', 'B', "D' F2 D R F2 D2 B D' L B L B2 D B F D' L2 B' R B R' F' R' F2 B2 R' U' R2 B U' L' B U B D2 R L2 B' R F2 D' F B2 R B' L R D2 L2 B2"),
-                     ('Y', 'R', "B L' F R' D2 B2 F' R' D L R2 F2 R U2 F D2 U L B2 L' D2 U F' B2 R2 B U' D' B2 L' F' B D2 R U2 L U' F' L F' U' R D' B2 D' F2 D2 U L' D' L2 R2 F D F2 D L' R' F D2 B L' D' L' U' L U2 D2 R B2 L' B2 L' B R'")]
+                     ('Y', 'R', "B L' F R' D2 B2 F' R' D L R2 F2 R U2 F D2 U L B2 L' D2 U F' B2 R2 B U' D' B2 L' F' B D2 R U2 L U' F' L F' U' R D' B2 D' F2 D2 U L' D' L2 R2 F D F2 D L' R' F D2 B L' D' L' U' L U2 D2 R B2 L' B2 L' B R'"),
+                     ('W', 'B', "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2")]
 
         for i, scramble in enumerate(scrambles, 1):
             self.scramble_assert(scramble[0], scramble[1], scramble[2], i)
