017d1bf7d06c76f1a7aaf54ea5d8c8d8483bbc4c
Fernando Soto <fjsoto1199@gmail.com>
2021-10-01 08:39:31 -0500

    1C updated

--- 1C.py
+++ 1C.py
@@ -8,6 +8,6 @@ sol, iters = bisection_method(f, 0, math.pi / 2)
 
 for k, v in iters.items():
     print(f"{k:4}\t\n"
-          f"\tL: {v['L']:<10.5f} ({f(v['L']):10.5f})\t\n"
-          f"\tU: {v['U']:<10.5f} ({f(v['U']):10.5f})\t\n"
-          f"\tM: {v['M']:<10.5f} ({f(v['M']):10.5f})")
+          f"\tL: {v['L']:<10.8f} ({f(v['L']):10.8f})\t\n"
+          f"\tU: {v['U']:<10.8f} ({f(v['U']):10.8f})\t\n"
+          f"\tM: {v['M']:<10.8f} ({f(v['M']):10.8f})")
