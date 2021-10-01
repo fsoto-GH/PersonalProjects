d26bddcde18d869ca6f11828a7a6a2405fc77a44
Fernando Soto <fjsoto1199@gmail.com>
2021-10-01 08:39:00 -0500

    s

--- 1A.py
+++ 1A.py
@@ -10,19 +10,20 @@ a = 0
 b = math.pi / 2
 x_0 = 0
 t = 10**-4
+max_iters = 5_000
 
 print(f"\tf(x)=sin(x) - 0.25")
 print("\t0 <= x <= pi/2")
 print('\tBisection Method')
 
-sol, iters = bisection_method(f, a, b, t=t)
+sol, iters = bisection_method(f, a, b, t=t, max_iters=max_iters)
 for k, v in iters.items():
     print(f"{k:4}\t\t\n"
           f"\t\tL: {v['L']:<10.8f} ({f(v['L']):11.8f})\t\n"
           f"\t\tU: {v['U']:<10.8f} ({f(v['U']):11.8f})\t\n"
           f"\t\tM: {v['M']:<10.8f} ({f(v['M']):11.8f})")
 
-sol, iters = false_position(f, a, b, t=t)
+sol, iters = false_position(f, a, b, t=t, max_iters=max_iters)
 print('False Position')
 for k, v in iters.items():
     print(f"{k:4}\t\t\n"
@@ -31,7 +32,7 @@ for k, v in iters.items():
           f"\t\tM: {v['M']:<10.8f} ({f(v['M']):11.8f})")
 
 print("Newton's Method")
-sol, iters = newton_method(f, fp, x_0=x_0, t=t)
+sol, iters = newton_method(f, fp, x_0=x_0, t=t, max_iters=max_iters)
 for k, v in iters.items():
     print(f"{k:4}\t\t\n"
           f"\t\tx: {v:10.8f} ({f(v):11.8f})\t")
